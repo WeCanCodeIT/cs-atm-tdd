354ad5e7da361f948d6577accadd6318dbe03c55
Jen E <jen@wecancodeit.org>
2019-09-11 10:29:32 -0400

    atm starter project

---
+++ ATM_TDD.Tests/ATMTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ATM_TDD.Tests
+{
+    public class ATMTests
+    {
+        // CheckBalance() should return balance of 200
+
+        // Withdrawal should reduce balance by 10 dollars
+
+        // Deposit should increase balance by 50 dollars
+    }
+}
