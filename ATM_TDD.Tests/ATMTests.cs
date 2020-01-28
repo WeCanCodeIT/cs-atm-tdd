93dad3a2f9153703b47b779f3d53963770025632
Jen E <jenevans3ten@gmail.com>
2020-01-28 12:14:47 -0500

    atm class with balance property and default value of 100

--- ATM_TDD.Tests/ATMTests.cs
+++ ATM_TDD.Tests/ATMTests.cs
@@ -5,6 +5,19 @@ namespace ATM_TDD.Tests
 {
     public class ATMTests
     {
+        // new account has balance of 0
+        [Fact]
+        public void New_ATM_Should_Have_Balance_100()
+        {
+            // Arrange
+            ATM myAtm = new ATM();
+
+            // Act
+
+            // Assert
+            Assert.Equal(100.00, myAtm.Balance);
+        }
+        
         // CheckBalance() should return balance of 200
 
         // Withdrawal should reduce balance by 10 dollars
---
+++ ATM_TDD/ATM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATM_TDD
+{
+    public class ATM
+    {
+        public double Balance { get; set; }
+
+        public ATM()
+        {
+            Balance = 100.00;
+        }
+    }
+}
