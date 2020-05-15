20e872eebec79f9be3a6df11d8931676bf4d80f5
Jen E <jenevans3ten@gmail.com>
2020-05-15 08:33:49 -0400

    cleared code from account class

--- ATM_TDD/Account.cs
+++ ATM_TDD/Account.cs
@@ -6,27 +6,6 @@ namespace ATM_TDD
 {
     public class Account
     {
-        public double Balance { get; set; }
-
-        public Account()
-        {
-            Balance = 100.00;
-        }
-
-        public double CheckBalance()
-        {
-            return Balance;
-        }
-
-        public void Withdraw()
-        {
-            Balance -= 10;
-            //Balance = Balance - 10;
-        }
-
-        public void Deposit()
-        {
-            Balance += 50;
-        }
+        
     }
 }
