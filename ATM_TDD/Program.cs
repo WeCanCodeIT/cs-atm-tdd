fb1c20e042eb249b72ab2e0e3b643da78fbbade3
Jen E <jenevans3ten@gmail.com>
2020-05-15 08:34:44 -0400

    cleared out program class

--- ATM_TDD/Program.cs
+++ ATM_TDD/Program.cs
@@ -6,15 +6,7 @@ namespace ATM_TDD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to ATM Machine");
 
-            Console.WriteLine("\nWhat would you like to do?");
-            Console.WriteLine("1. Check balance");
-            Console.WriteLine("2. Withdraw $10");
-            Console.WriteLine("3. Deposit $50");
-            Console.WriteLine("4. Exit");
-
-            string atmChoice = Console.ReadLine();
         }
     }
 }
